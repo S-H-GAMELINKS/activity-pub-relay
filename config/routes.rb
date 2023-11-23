006d60dfe7558e023e48916b6778cdb2618e85c5
S-H-GAMELINKS <gamelinks007@gmail.com>
2023-11-23 21:42:47 +0900

    Mount sidekiq web ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -10,6 +12,8 @@ Rails.application.routes.draw do
   resource :actor, only: :show
 
   get '/.well-known/webfinger', to: 'webfinger#show'
+
+  mount Sidekiq::Web => "/sidekiq"
 
   # Defines the root path route ("/")
   # root "posts#index"
