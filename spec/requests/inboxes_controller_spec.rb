b60502bea35d0a87840b59e2feea87db3ccfe54e
S-H-GAMELINKS <gamelinks007@gmail.com>
2024-11-13 23:35:08 +0900

    Migrate /inbox spec

---
+++ spec/requests/inboxes_controller_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe "/inbox", type: :request do
+  describe "POST /inbox" do
+    before do
+      post "/inbox"
+    end
+
+    it "should return 202" do
+      expect(response.status). to eq 202
+    end
+  end
+end
