792b9288f4d6f007b5ab50cefe05c559f9a26e5f
S-H-GAMELINKS <gamelinks007@gmail.com>
2024-11-14 12:17:31 +0900

    Introduce LOCAL_DOMAIN environment variable

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,13 +2,13 @@ class ActorsController < ApplicationController
   def show
     render json: {
       "@context": %w[https://www.w3.org/ns/activitystreams https://w3id.org/security/v1],
-      id: "https://www.example.com/actor",
+      id: "https://#{ENV.fetch("LOCAL_DOMAIN", "www.example.com")}/actor",
       type: "Service",
       preferredUsername: "relay",
-      inbox: "https://www.example.com/inbox",
+      inbox: "https://#{ENV.fetch("LOCAL_DOMAIN", "www.example.com")}/inbox",
       publicKey: {
-        id: "https://www.example.com/#main-key",
-        owner: "https://www.example.com",
+        id: "https://#{ENV.fetch("LOCAL_DOMAIN", "www.example.com")}/#main-key",
+        owner: "https://#{ENV.fetch("LOCAL_DOMAIN", "www.example.com")}",
         publicKeyPem: Actor.key.public_key.to_pem
       }
     }, content_type: "application/activity+json"
