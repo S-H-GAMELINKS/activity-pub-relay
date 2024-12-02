a1ac9fbe000b837963aa34e4ca13af1c547f0827
S-H-GAMELINKS <gamelinks007@gmail.com>
2024-12-02 20:14:49 +0900

    Setup Rodauth for Session management

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_02_094946) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_02_095810) do
+  create_table "account_login_change_keys", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "login", null: false
+    t.datetime "deadline", null: false
+  end
+
+  create_table "account_password_reset_keys", force: :cascade do |t|
+    t.string "key", null: false
+    t.datetime "deadline", null: false
+    t.datetime "email_last_sent", default: -> { "CURRENT_TIMESTAMP" }, null: false
+  end
+
+  create_table "account_remember_keys", force: :cascade do |t|
+    t.string "key", null: false
+    t.datetime "deadline", null: false
+  end
+
+  create_table "account_verification_keys", force: :cascade do |t|
+    t.string "key", null: false
+    t.datetime "requested_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "email_last_sent", default: -> { "CURRENT_TIMESTAMP" }, null: false
+  end
+
+  create_table "accounts", force: :cascade do |t|
+    t.integer "status", default: 1, null: false
+    t.string "email", null: false
+    t.string "password_hash"
+    t.index ["email"], name: "index_accounts_on_email", unique: true, where: "status IN (1, 2) /*application='ActivityPubRelay'*/"
+  end
+
   create_table "subscribe_servers", force: :cascade do |t|
     t.string "domain", default: "", null: false
     t.string "inbox_url", default: "", null: false
@@ -18,4 +48,9 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_02_094946) do
     t.datetime "updated_at", null: false
     t.index ["domain"], name: "index_subscribe_servers_on_domain", unique: true
   end
+
+  add_foreign_key "account_login_change_keys", "accounts", column: "id"
+  add_foreign_key "account_password_reset_keys", "accounts", column: "id"
+  add_foreign_key "account_remember_keys", "accounts", column: "id"
+  add_foreign_key "account_verification_keys", "accounts", column: "id"
 end
