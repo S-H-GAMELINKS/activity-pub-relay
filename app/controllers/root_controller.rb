afa8a17dc52add8002b0ec2569e303b0b7fd8ab6
S-H-GAMELINKS <gamelinks007@gmail.com>
2024-12-07 14:39:00 +0900

    Introduce Daily Rebroadcast Counter Graph

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,5 +1,10 @@
 class RootController < ApplicationController
   def index
     @subscribe_server_domains = SubscribeServer.pluck(:domain)
+    @rebroadcast_counters = 7.downto(0).map do |num|
+      key = num.days.ago.strftime("%Y-%m-%d")
+
+      [ key, Rails.cache.read(key) ]
+    end
   end
 end
