28b4c43edcf82a3d22a46959ef2b276c6d8ea67b
S-H-GAMELINKS <gamelinks007@gmail.com>
2024-12-07 14:56:51 +0900

    Set default value for when key is not exist

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -3,8 +3,9 @@ class RootController < ApplicationController
     @subscribe_server_domains = SubscribeServer.pluck(:domain)
     @rebroadcast_counters = 7.downto(0).map do |num|
       key = num.days.ago.strftime("%Y-%m-%d")
+      value = Rails.cache.read(key)
 
-      [ key, Rails.cache.read(key) ]
+      [ key, value ? value : 0 ]
     end
   end
 end
